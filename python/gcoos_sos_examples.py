4eb6657ef78a2fa7a328e7a9b76937d65b38cb25
Bob Currier <robertdcurrier@gmail.com>
2015-12-17 11:35:17 -0500

    Changed to using ConfigParser

--- python/gcoos_sos_examples.py
+++ python/gcoos_sos_examples.py
@@ -2,49 +2,49 @@
 """
 DOCSTRING
 """
-
 from geojson import FeatureCollection
-from gcoos_sos_utils import gcoos_get_all_stations, gcoos_describe_sensor
-def get_all_stations():
+from gcoos_sos_utils import (gcoos_get_all_stations, gcoos_describe_sensor,
+                             gcoos_get_capabilities)
+def get_capabilities(r_a):
+    """
+    DOCSTRING
+    """
+    print gcoos_get_capabilities(r_a)
+
+def get_all_stations(r_a):
     """
     DOCSTRING
     """
     #Show all stations in catalog
-    print "Getting all stations..."
-    for station in gcoos_get_all_stations():
+    for station in gcoos_get_all_stations(r_a):
         print station
-    print "--------------------------------"
 
-def get_single_sensor():
+def get_single_sensor(r_a, urn):
     """
     DOCSTRING
     """
     #Get single station description as GeoJSON
     #simple text output
-    print "Getting single station..."
-    print gcoos_describe_sensor('urn:ioos:station:nerrs:apaebwq')
-    print "--------------------------------"
+    print gcoos_describe_sensor(r_a, urn)
 
-def get_station_total():
+def get_station_total(r_a):
     """
     DOCSTRING
     """
     #Total stations in catalog
-    print "Total stations: %d" % len(gcoos_get_all_stations())
-    print "--------------------------------"
+    print "Total stations: %d" % len(gcoos_get_all_stations(r_a))
 
-def get_all_sensors():
+def get_all_sensors(r_a):
     """
     DOCSTRING
     """
     #Get all sensor descriptions as GeoJSON features
     #simple text output
-    for station in gcoos_get_all_stations():
+    for station in gcoos_get_all_stations(r_a):
         print "Fetching data for %s..." % station
-        print gcoos_describe_sensor(station)
-    print "--------------------------------"
+        print gcoos_describe_sensor(r_a, station)
 
-def get_all_sensors_fc():
+def get_all_sensors_fc(r_a):
     """
     DOCSTRING
     """
@@ -52,13 +52,12 @@ def get_all_sensors_fc():
     #and make into GeoJSON feature collection that can
     #be dropped into Leaflet map
     the_collection = []
-    for station in gcoos_get_all_stations():
-        print "Fetching data for %s..." % station
-        the_collection.append(gcoos_describe_sensor(station))
+    for station in gcoos_get_all_stations(r_a):
+        the_collection.append(gcoos_describe_sensor(r_a, station))
     the_fc = FeatureCollection(the_collection)
     print the_fc
 
-#Uncomment the example you wish to execute
-#get_all_stations()
-#get_single_sensor()
-#get_all_sensors_fc()
+if __name__ == '__main__':
+    #get_station_total('gcoos')
+    #get_single_sensor('gcoos', 'urn:ioos:station:nerrs:apaebwq')
+    get_all_sensors_fc('gcoos')
--- python/gcoos_sos_utils.py
+++ python/gcoos_sos_utils.py
@@ -12,66 +12,74 @@ Notes:      GCOOS Python Browse SOS. The example methods are intended
             http://data.gcoos.org/52N_SOS.php.  We use BeautifulSoup4
             to parse the XML as good soup is a wonderous thing.
 """
+import ConfigParser
 import urllib2
 from bs4 import BeautifulSoup as soup
 from geojson import Feature, Point
-def gcoos_get_capabilities():
+
+def gcoos_get_capabilities(r_a):
     """
     Notes:
     We use a hard-wired url. Ideally, we would use ConfigParser
     and retrieve the URL for each RA from a config file.
     """
-    url = 'http://data.gcoos.org:8080/52nSOS/sos?REQUEST=GetCapabilities\
-&SERVICE=SOS&ACCEPTVERSIONS=1.0.0'
-    the_file = urllib2.urlopen(url)
+    the_server = CONFIG.get('ra_servers', r_a)
+    base_url = CONFIG.get('base_urls', 'get_capabilities')
+    the_url = the_server + base_url
+    if DEBUG:
+        print "gcoos_get_capabilities('%s')" % r_a
+
+    the_file = urllib2.urlopen(the_url)
     the_data = the_file.read()
     the_file.close()
     return soup(the_data, 'html.parser')
 
-def gcoos_get_observed_properties():
+def gcoos_get_observed_properties(r_a):
     """
     Notes:
     BeautifulSoup parses the entire tree and returns
     all observed properties.
     """
+    if DEBUG:
+        print "gcoos_get_observed_properties('%s')" % r_a
+
     observed_props = []
-    the_soup = gcoos_get_capabilities()
+    the_soup = gcoos_get_capabilities(r_a)
     properties = the_soup.find_all('sos:observedproperty')
     for the_property in properties:
         observed_props.append(the_property)
     return set(observed_props)
 
-def gcoos_get_all_stations():
+def gcoos_get_all_stations(r_a):
     """
     Notes:
     Gets a list of all stations and returns the uri
     for use in gcoos_describe_sensor and gcoos_get_observation
     """
+    if DEBUG:
+        print "gcoos_get_all_stations('%s')" % r_a
     the_stations = []
-    the_soup = gcoos_get_capabilities()
+    the_soup = gcoos_get_capabilities(r_a)
     the_soup = (the_soup.find_all('gml:name'))
     for station in the_soup:
         the_stations.append(station.contents[0])
     return the_stations
 
-def gcoos_describe_sensor(urn):
+def gcoos_describe_sensor(r_a, urn):
     """
     Notes:
-    Hardwired url for this demo -- very ugly but we ran out
-    of time. Will work on making this much more pythonic.
-    Hard to embed parameters with all the quotable chars...
-    [TO DO] Change the concat to a simple string.replace()
-    and we should be good to go.
+    We get all settings from the .cfg file and build
+    the_url. Different RAs are running different versions
+    of SOS so the XML parsing might need some tweaking. This
+    code is known to work with the GCOOS-RA SOS server.
     """
-    url = 'http://data.gcoos.org:8080/52nSOS/sos/kvp?service=SOS&'
-    url = url + 'version=1.0.0&request=DescribeSensor&procedure='
-    url = url + urn + '&outputFormat=text%2Fxml%3B%20subtype%3D%22'
-    url = url + 'sensorML%2F1.0.1%2Fprofiles%2Fioos_sos%2F1.0%22'
+    the_url = CONFIG.get('ra_servers', r_a)
+    the_url = the_url + CONFIG.get('base_urls', 'describe_sensor')
+    the_url = the_url.replace('[anyURI]', urn)
+    if DEBUG:
+        print "gcoos_describe_sensor(%s, %s)..." % (r_a, urn)
 
-    the_file = urllib2.urlopen(url)
-    the_data = the_file.read()
-    the_file.close()
-    the_soup = soup(the_data, 'html.parser')
+    the_soup = soup(urllib2.urlopen(the_url).read(), 'html.parser')
     #get position
     the_pos = the_soup.find('gml:pos').contents[0]
     latitude = float(the_pos.split(' ')[0])
@@ -101,4 +109,8 @@ def gcoos_get_org_stations(org):
     Notes:
     """
     print org
+CONFIG = ConfigParser.ConfigParser()
+CONFIG_FILE = './gcoos_sos_utils.cfg'
+CONFIG.readfp(open(CONFIG_FILE))
+DEBUG = CONFIG.getboolean('settings', 'debug')
 
